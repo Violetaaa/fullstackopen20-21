c964aa842830977c0fbb2a38070b587bb551ddc7
Violeta <valvarezareces@gmail.com>
2020-08-12 13:33:00 +0200

    1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,22 +5,9 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const Average = ({good, bad, neutral}) => {
-  if(good+bad+neutral<=0){
-    return 0
-  } else {
-    return (good-bad)/(good+bad+neutral)
-  }
-}
-
-const Positive = ({good, bad, neutral}) => {
-  if(good+bad+neutral<=0){
-    return 0
-  } else {
-    return (good*100)/(good+bad+neutral)
-  }
-}
- 
+const Statistics = ({ text, value }) => (
+  <p>{text}{value}</p>
+)
 
 const App = () => {
   // save clicks of each button to own state
@@ -28,31 +15,47 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const goodVote = () =>  setGood(good + 1)
-  const neutralVote = () =>  setNeutral(neutral + 1)
-  const badVote = () =>  setBad(bad + 1)
- 
+  const averageCalculator = () => {
+    if (good + bad + neutral <= 0) {
+      return 0
+    } else {
+      return (good - bad) / (good + bad + neutral)
+    }
+  }
+
+  const positiveCalculator = () => {
+    if (good + bad + neutral <= 0) {
+      return 0
+    } else {
+      return ((good * 100) / (good + bad + neutral)) + '%'
+    }
+  }
+
+  const goodVote = () => setGood(good + 1)
+  const neutralVote = () => setNeutral(neutral + 1)
+  const badVote = () => setBad(bad + 1)
+
   return (
     <div>
       <h1>give feedback</h1>
       <div>
-        <Button handleClick={goodVote} text='good'/>
-	      <Button handleClick={neutralVote} text='neutral' />           
-	      <Button handleClick={badVote} text='bad' />
+        <Button handleClick={goodVote} text='good' />
+        <Button handleClick={neutralVote} text='neutral' />
+        <Button handleClick={badVote} text='bad' />
       </div>
       <h1>statistics</h1>
       <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {good+neutral+bad}</p>
-        <p>average <Average good={good} neutral={neutral} bad={bad}/></p>
-        <p>positive <Positive good={good} neutral={neutral} bad={bad}/></p>
+        <Statistics text='good: ' value={good} />
+        <Statistics text='neutral: ' value={neutral} />
+        <Statistics text='bad: ' value={bad} />
+        <Statistics text='all: ' value={good + neutral + bad} />
+        <Statistics text='average: ' value={averageCalculator()} />
+        <Statistics text='positive: ' value={positiveCalculator()} />
       </div>
     </div>
   )
 }
 
-ReactDOM.render(<App />, 
+ReactDOM.render(<App />,
   document.getElementById('root')
 )
