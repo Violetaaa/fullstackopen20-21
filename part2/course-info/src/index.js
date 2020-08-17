264d881e89b83cc0100afa4c8827109b512605be
Violeta <valvarezareces@gmail.com>
2020-08-17 18:11:09 +0200

    2.4

--- part2/course-info/src/index.js
+++ part2/course-info/src/index.js
@@ -2,27 +2,55 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map(course => (<Course kery={course.id} course={course} />))}
     </div>
   )
 }
@@ -33,7 +61,7 @@ const Course = ({ course }) => {
       <Header name={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
-  </div>
+    </div>
   )
 }
 
@@ -41,7 +69,7 @@ const Course = ({ course }) => {
 const Header = (props) => {
   return (
     <div>
-       <h1>{props.name}</h1>
+      <h1>{props.name}</h1>
     </div>
   )
 }
@@ -49,20 +77,20 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-     {props.parts.map(part => (<Part key={part.id} part={part}/>))}
+      {props.parts.map(part => (<Part key={part.id} part={part} />))}
     </div>
-  ) 
+  )
 
 }
 
 const Part = (props) => {
   // console.log(props)
   return (
-      <div>
-         <p>
-          {props.part.name} {props.part.exercises}
-        </p>
-      </div>
+    <div>
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+    </div>
   )
 }
 
