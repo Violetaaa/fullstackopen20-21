bbb7775a3f1390283eeedead52c83304d3fe3e41
Violeta <valvarezareces@gmail.com>
2020-10-20 12:51:26 +0200

    4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,11 +3,29 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-  const result = blogs.reduce((sum, blog) => sum + blog.likes, 0)
-  return result
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+
+  if (blogs.length === 0) {
+    return 'No blogs rated yet'
+  }
+
+  return blogs.reduce((a, b) => a.likes > b.likes ? a : b)
+
+  // maxLikes = -1
+  // for (var i = 0; i < blogs.length; i++) {
+  //   if (blogs[i]['likes'] > maxLikes){
+  //     maxLikes = blogs[i]['likes'] 
+  //     mostLikedBlog = blogs[i]
+  //   }
+  // }
+  // return mostLikedBlog
 }
 
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
+
 
