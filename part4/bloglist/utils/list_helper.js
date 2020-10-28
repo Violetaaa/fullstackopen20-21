cc3765bb8c509ed09c7a4f86ccb798f94aba3d77
Violeta <valvarezareces@gmail.com>
2020-10-28 19:28:19 +0100

    4.8

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,31 +1,16 @@
-const dummy = (blogs) => {
-  return 1
-}
+const _ = require("lodash");
 
-const totalLikes = (blogs) => {
-  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
-}
+const dummy = blogs => 1
 
-const favoriteBlog = (blogs) => {
+const totalLikes = blogs => blogs.reduce((sum, blog) => sum + blog.likes, 0)
 
+const favoriteBlog = blogs => {
   if (blogs.length === 0) {
     return 'No blogs rated yet'
   }
-
   return blogs.reduce((a, b) => a.likes > b.likes ? a : b)
-
-  // maxLikes = -1
-  // for (var i = 0; i < blogs.length; i++) {
-  //   if (blogs[i]['likes'] > maxLikes){
-  //     maxLikes = blogs[i]['likes'] 
-  //     mostLikedBlog = blogs[i]
-  //   }
-  // }
-  // return mostLikedBlog
 }
 
 module.exports = {
   dummy, totalLikes, favoriteBlog
 }
-
-
